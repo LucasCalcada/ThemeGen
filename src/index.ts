d6e85a841aadbec289b5e159f4ec4e97997a5eed
Lucas Calçada <lucasmgmcalcada@gmail.com>
2025-02-19 20:40:54 -0300

    lint index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import { parse } from "yaml";
-import { readFileSync, writeFileSync, existsSync, mkdirSync } from "fs"
+import { readFileSync, writeFileSync, existsSync, mkdirSync } from "fs";
 import path from "path";
 import mustache from "mustache";
 
 interface TemplateFile {
-  template: string,
-  outputPath: string
+  template: string;
+  outputPath: string;
 }
 
 const CONFIG_PATH = "./config.yaml";
@@ -15,9 +15,9 @@ const THEME_PATH = "./colorscheme.yaml";
 const THEME = parse(readFileSync(THEME_PATH, "utf8"));
 
 // Create colorArray property from theme colors
-THEME.colorArray = []
+THEME.colorArray = [];
 let colorKeys = Object.keys(THEME.colors);
-colorKeys.map(key => {
+colorKeys.map((key) => {
   THEME.colorArray.push({ name: key, value: THEME.colors[key] });
 });
 
@@ -26,7 +26,10 @@ const GENERATED_PATH = CONFIG.generatedPath ?? "./generated/";
 const templates: TemplateFile[] = CONFIG.generatedFiles;
 
 templates.map((template: TemplateFile) => {
-  let templatePath = path.resolve(__dirname, `./templates/${template.template}.mustache`);
+  let templatePath = path.resolve(
+    __dirname,
+    `./templates/${template.template}.mustache`,
+  );
   let templateFile = readFileSync(templatePath, "utf-8");
   let compiledFile = mustache.render(templateFile, THEME);
   let outputPath = path.join(GENERATED_PATH, template.outputPath);
@@ -35,7 +38,7 @@ templates.map((template: TemplateFile) => {
 
   if (CONFIG.debug) {
     console.log(compiledFile);
-    return
+    return;
   }
   writeFileSync(outputPath, compiledFile);
 });
