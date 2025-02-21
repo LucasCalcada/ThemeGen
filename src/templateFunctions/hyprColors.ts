5bc5b2686aa8aebbde671c287a36076690626ab4
Lucas Calçada <lucasmgmcalcada@gmail.com>
2025-02-20 22:23:47 -0300

    fix: fix alpha value calculation

--- src/templateFunctions/hyprColors.ts
+++ src/templateFunctions/hyprColors.ts
@@ -1,4 +1,4 @@
-const hex: RegExp = new RegExp("#[0-9a-fA-F]{6}");
+const hex: RegExp = new RegExp("#[0-9a-fA-F]{6,8}");
 export default function hexToHypr(text: string, render: Function) {
   text = render(text);
   text = text.trim();
@@ -13,5 +13,15 @@ export default function hexToHypr(text: string, render: Function) {
     let hexValue: string = text.slice(i, i + 2);
     values.push(parseInt(hexValue, 16));
   }
+
+  if (text.length > 6) {
+    let alpha = parseInt(text.slice(6, 8), 16);
+    alpha = alpha / 255;
+    alpha = Math.floor(alpha * 100) / 100;
+    values.push(alpha);
+  } else {
+    values.push(1);
+  }
+
   return values.join(", ");
 }
